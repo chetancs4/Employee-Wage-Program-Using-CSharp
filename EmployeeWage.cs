c310129b30cdc75b72d4444d54f640b4a57d0a0c
Chetan <chetansadvelkar4@gmail.com>
2023-07-07 10:01:22 +0530

    Chetan: Calculate working Hours

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -4,15 +4,18 @@
     {
         public void CalculateEmployeeWage()
         {
-            //Calculate Monthly Wages
+
+            //Working Hour Conditions Using While Loop
             Random random = new Random();
-            int Salary = 0, daily_hour = 0, total_working_hour = 0, monthly_salary = 0;
+            int salary = 0, daily_hour = 0, total_working_hour = 0, monthly_salary = 0, day = 1;
+            //Constant variables
             const byte IS_PRESENT_FULL_TIME = 1;
             const byte IS_PRESENT_PART_TIME = 2;
             const byte WAGE_PER_HOUR = 20;
             const byte TOTAL_WORKING_DAYS = 20;
+            const byte MAX_WORKING_HOURS = 100;
 
-            for (int day = 1; day <= TOTAL_WORKING_DAYS; day++)
+            while (day <= TOTAL_WORKING_DAYS && total_working_hour <= MAX_WORKING_HOURS)
             {
                 int attendance = random.Next(0, 3);
                 switch (attendance)
@@ -33,17 +36,19 @@
                         break;
                 }
                 //Calculating salary of Employee
-                Salary = daily_hour * WAGE_PER_HOUR;
-                Console.WriteLine("Salary of Employee is Rs." + Salary);
+                salary = daily_hour * WAGE_PER_HOUR;
+                Console.WriteLine("Salary of Employee is Rs." + salary);
 
                 //Calculating total working hour of Employee
                 total_working_hour = total_working_hour + daily_hour;
-                Console.WriteLine("Total working hours: " + total_working_hour);
+                Console.WriteLine("Total working hours : " + total_working_hour);
+                day++;
             }
-            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("--------------------------------------------");
             //Calculating Monthly salary of Employee
             monthly_salary = total_working_hour * WAGE_PER_HOUR;
-            Console.WriteLine("Monthly salary of Employee is Rs: " + monthly_salary);
+            Console.WriteLine("Monthly salary of Employee is Rs." + monthly_salary);
+
             Console.ReadLine();
 
 
