7d313c9a1b518be6cb436987d3a75b117bcb2452
Chetan <chetansadvelkar4@gmail.com>
2023-07-07 09:28:32 +0530

    Chetan: This is Employee Wage Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Computation Program");
+            Console.WriteLine("--------------------------------------------");
+
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.CalculateEmployeeWage();
+        }
+    }
+}
