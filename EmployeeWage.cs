2a3d262236cb20feaaae61b32cb9cfbcdab418e8
Chetan <chetansadvelkar4@gmail.com>
2023-07-07 09:37:03 +0530

    Chetan: Calculate Part time Wages

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -5,23 +5,33 @@
         public void CalculateEmployeeWage()
         {
             Random random = new Random();
-            int attendance = random.Next(0, 2);
+            int attendance = random.Next(0, 3);
 
-            int Salary = 0;
+            int salary = 0;
 
             //Constant variables
+            const byte IS_PRESENT_FULL_TIME = 1;
+            const byte IS_PRESENT_PART_TIME = 2;
             const byte FULL_DAY_HOUR = 8;
+            const byte PART_TIME_HOUR = 4;
             const byte WAGE_PER_HOUR = 20;
 
-            //Calculate Employee Present Salary and Absent Salary.
-            if (attendance == 1)
+            //Checking if the Employee is Present or not
+            if (attendance == IS_PRESENT_FULL_TIME)
             {
-                Salary = FULL_DAY_HOUR * WAGE_PER_HOUR;
-                Console.WriteLine("Employee is Present for Full time and the Salary is Rs: " + Salary);
+                //Calculating the salary for Full time Employee
+                salary = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine("Employee is Present for Full time and the Salary is Rs." + salary);
+            }
+            else if (attendance == IS_PRESENT_PART_TIME)
+            {
+                //Calculating the salary for Part time Employee
+                salary = PART_TIME_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine("Employee is Present for Part time and the Salary is Rs." + salary);
             }
             else
             {
-                Console.WriteLine("Employee is Absent and the Salary is Rs: " + Salary);
+                Console.WriteLine("Employee is Absent and the Salary is Rs." + salary);
             }
             Console.ReadLine();
 
